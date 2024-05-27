872979138763a41ae465174cf7ae5aed3709914d
Delta8142 <jordan.wijaya888@gmail.com>
2024-05-27 20:33:59 +0700

    commit message here

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -88,9 +88,10 @@ const Home = ({setSelectedPage}: Props) => {
             <div className="h-[150px] w-full bg-brown-100 py-10">
                 <div className="mx-auto w-5/6">
                     <div className="flex w-3/5 items-center justify-between gap-8">
-                    <img alt="redbull-sponsor" src={SponsorRedBull} />
+                        {/* IMAGE SPONSOR */}
+                    {/* <img alt="redbull-sponsor" src={SponsorRedBull} />
                     <img alt="forbes-sponsor" src={SponsorForbes} />
-                    <img alt="fortune-sponsor" src={SponsorFortune} />
+                    <img alt="fortune-sponsor" src={SponsorFortune} /> */}
                     </div>
                 </div>
             </div>
